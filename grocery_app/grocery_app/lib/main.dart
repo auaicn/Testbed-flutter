f0c1cc7b2696a1678d9725a6205135b90ab913e3
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-08-24 19:45:42 +0900

    Init: Added constants

--- grocery_app/grocery_app/lib/main.dart
+++ grocery_app/grocery_app/lib/main.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/constants.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(GroceryApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class GroceryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: primaryColor,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Groceries'),
     );
   }
 }

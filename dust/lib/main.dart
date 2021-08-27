3144897daeb7358893eaf18c2904d51aedbd03bf
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-08-27 14:34:18 +0900

    Init: In-course `Dust` application
    - with google-fonts plug-in

---
+++ dust/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        fontFamily: GoogleFonts.acme().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dust"),
+      ),
+      body: Center(
+        child: Text(
+          "text",
+        ),
+      ),
+    );
+  }
+}

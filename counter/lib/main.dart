7b8b12590d4195b36753065246c3ba6ac143f592
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-08-28 15:28:55 +0900

    Init: Added project repository for Counter Application

---
+++ counter/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(title: 'Counter App'),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

50c39ce66effe83da9b6587c4b7e53d4921bde92
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-08-29 00:10:45 +0900

    Init: Added project for BLoC pattern
    - BLoC is abbreviation for "Business Logic Component"

---
+++ bloc_pattern/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(title: 'Bloc Pattern'),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}

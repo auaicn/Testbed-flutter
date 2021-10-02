6f1ec512ddfb602afa785c9d32cf58d312ccc7e4
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-10-03 01:02:13 +0900

    Feat: Added single country card

--- analog_clock/lib/components/country_card.dart
+++ analog_clock/lib/components/country_card.dart
@@ -1,38 +1,6 @@
-import 'package:analog_clock/components/clock.dart';
 import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
-import 'time_in_hour_and_minute.dart';
-
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'NewPort Beach, USA | PST',
-            style: Theme.of(context).textTheme.bodyText1,
-          ),
-          TimeInHourAndMinute(),
-          Spacer(),
-          Clock(),
-          Spacer(),
-          CountryCard(
-            country: 'New York, USA',
-            iconSrc: 'assets/icons/Liberty.svg',
-            period: 'AM',
-            time: '9:20',
-            timeZone: '+3 HRS | EST',
-          )
-        ],
-      ),
-    );
-  }
-}
 
 class CountryCard extends StatelessWidget {
   const CountryCard({

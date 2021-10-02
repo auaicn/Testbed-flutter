6e1dbb333e9d39297a833d1222e372b86d57e68f
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-10-02 15:59:43 +0900

    Minor: added themes
    - dark theme
    - light theme
    - appbar theme

--- analog_clock/lib/size_config.dart
+++ analog_clock/lib/size_config.dart
@@ -14,3 +14,16 @@ class SizeConfig {
     orientation = _mediaQueryData.orientation;
   }
 }
+
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+
+  /// 896.0 is height that designer uses. or we can say it's iphone 11
+  return (inputHeight / 896.0) * screenHeight;
+}
+
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+
+  return (inputWidth / 414.0) * screenWidth;
+}

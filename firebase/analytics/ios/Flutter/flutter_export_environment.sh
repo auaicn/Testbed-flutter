663c69ad2d4c9f592e465c5b9ac86de0e584fff2
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-11-29 22:57:22 +0900

    Chore: Changed iOS Target Deployment Version to 10.0 - to meet cloud_firestore minimum requiremnts - It seems to be applied to pbxproj file

--- firebase/analytics/ios/Flutter/flutter_export_environment.sh
+++ firebase/analytics/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/apple/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/apple/projects/flutter-testbed/analycis-starting"
+export "FLUTTER_APPLICATION_PATH=/Users/apple/projects/flutter-testbed/firebase/analytics"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/apple/projects/flutter-testbed/firebase/analytics/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/apple/projects/flutter-testbed/firebase/analytics/.dart_tool/package_config.json"
